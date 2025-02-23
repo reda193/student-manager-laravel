798d370ad00faace7d8c4669481b19856073caa4
reda193 <mohamed.reda33@outlook.com>
2025-02-23 11:57:19 -0500

    Working now, had to use github forums to debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
     <!-- Footer -->
     <footer class="bg-gray-800 text-white py-4">
         <div class="container mx-auto px-4 text-center">
-            <p>&copy; {{ date('Y') }} Student Management System. All rights reserved.</p>
+            <p>&copy; {{ date('Y') }} Student Managementdasd System. All rights reserved.</p>
         </div>
     </footer>
 </body>
